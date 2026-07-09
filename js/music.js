6c137ed801a971ba12896ea98805111ea0ca166d
Akutya77 <artificial.dr77@gmail.com>
2026-07-09 14:36:53 +0200

    Delete js directory

--- js/music.js
+++
@@ -1,2 +0,0 @@
-const a=new Audio('assets/music/romantic-piano.mp3');a.loop=true;
-document.getElementById('musicBtn').onclick=()=>a.paused?a.play():a.pause();
--- js/script.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('startBtn').onclick=()=>window.scrollTo({top:window.innerHeight,behavior:'smooth'});
-document.getElementById('themeBtn').onclick=()=>document.body.classList.toggle('light');
